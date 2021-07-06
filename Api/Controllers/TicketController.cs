df6c757d67807f2858faed9d2ca9afae3eb907fd
Saeed Nemati <sn.1361@gmail.com>
2021-07-06 11:34:09 +0430

    user is not up yet

--- Api/Controllers/TicketController.cs
+++ Api/Controllers/TicketController.cs
@@ -9,37 +9,37 @@ namespace Holism.Ticketing.Api.Controllers
     {
         public override ReadBusiness<Ticket> ReadBusiness => new TicketBusiness();
         
-        public long UserId
-        {
-            get
-            {
-                return (long)new SecureController().ParsedUserData(HttpContext).UserId;
-                return 1;
-            }
-        }
+        // public long UserId
+        // {
+        //     get
+        //     {
+        //         return (long)new SecureController().ParsedUserData(HttpContext).UserId;
+        //         return 1;
+        //     }
+        // }
 
-        public override Action<ListOptions> ListOptionsAugmenter => listOptions =>
-        {
-            Guid? userGuid = new UserBusiness().GetGuid(UserId);
-            listOptions.AddFilter<TicketView>(i => i.UserGuid, userGuid.ToString());
-        };
+        // public override Action<ListOptions> ListOptionsAugmenter => listOptions =>
+        // {
+        //     Guid? userGuid = new UserBusiness().GetGuid(UserId);
+        //     listOptions.AddFilter<TicketView>(i => i.UserGuid, userGuid.ToString());
+        // };
 
-        public override ViewBusiness<TicketView> ViewBusiness => new TicketBusiness();
+        // public override ViewBusiness<TicketView> ViewBusiness => new TicketBusiness();
 
-        [HttpPost]
-        public virtual IActionResult Create(Ticket ticket)
-        {
-            Guid? userGuid = new UserBusiness().GetGuid(UserId);
-            var createdEntity = new TicketBusiness().CreateTicket(ticket, userGuid.Value);
-            return OkJson("Done", createdEntity, "CretionDone");
-        }
+        // [HttpPost]
+        // public virtual IActionResult Create(Ticket ticket)
+        // {
+        //     Guid? userGuid = new UserBusiness().GetGuid(UserId);
+        //     var createdEntity = new TicketBusiness().CreateTicket(ticket, userGuid.Value);
+        //     return OkJson("Done", createdEntity, "CretionDone");
+        // }
 
-        [HttpPost]
-        public IActionResult Close(long ticketId)
-        {
-            new TicketBusiness().CloseTicket(ticketId);
-            return OkJson();
-        }
+        // [HttpPost]
+        // public IActionResult Close(long ticketId)
+        // {
+        //     new TicketBusiness().CloseTicket(ticketId);
+        //     return OkJson();
+        // }
 
     }
 }
