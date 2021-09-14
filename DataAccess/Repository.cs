41922f86ec28710a0519f7a596b1675c610db778
Saeed Nemati <sn.1361@gmail.com>
2021-09-14 17:02:43 +0430

    committed by script

--- DataAccess/Repository.cs
+++ DataAccess/Repository.cs
@@ -1,80 +1,71 @@
-using Holism.Ticketing.Models;
 using Holism.DataAccess;
+using Holism.Ticketing.Models;
 
 namespace Holism.Ticketing.DataAccess
 {
     public class Repository
     {
-        
         public static Repository<State> State
         {
             get
             {
-                return new Holism.DataAccess.Repository<State>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<State
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<Priority> Priority
         {
             get
             {
-                return new Holism.DataAccess.Repository<Priority>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<Priority
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<Ticket> Ticket
         {
             get
             {
-                return new Holism.DataAccess.Repository<Ticket>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<Ticket
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<Post> Post
         {
             get
             {
-                return new Holism.DataAccess.Repository<Post>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<Post
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<PostHtml> PostHtml
         {
             get
             {
-                return new Holism.DataAccess.Repository<PostHtml>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<PostHtml
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<AttachedFile> AttachedFile
         {
             get
             {
-                return new Holism.DataAccess.Repository<AttachedFile>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<AttachedFile
+                >(new TicketingsContext());
             }
         }
-    
-
 
         public static Repository<Goal> Goal
         {
             get
             {
-                return new Holism.DataAccess.Repository<Goal>(new TicketingsContext());
+                return new Holism.DataAccess.Repository<Goal
+                >(new TicketingsContext());
             }
         }
-    
-
-
     }
 }
--- DataAccess/TicketingContext.cs
+++ DataAccess/TicketingContext.cs
@@ -1,26 +1,23 @@
-public class TicketingContext : DatabaseContext 
-    {
-        public override string ConnectionStringName => "Ticketing";   
+public class TicketingContext : DatabaseContext
+{
+    public override string ConnectionStringName => "Ticketing";
 
-                         
-        public DbSet<State> States { get; set; } 
-                    
-        public DbSet<Priority> Prioritys { get; set; } 
-                    
-        public DbSet<Ticket> Tickets { get; set; } 
-                    
-        public DbSet<Post> Posts { get; set; } 
-                    
-        public DbSet<PostHtml> PostHtmls { get; set; } 
-                    
-        public DbSet<AttachedFile> AttachedFiles { get; set; } 
-                    
-        public DbSet<Goal> Goals { get; set; } 
-     
-        
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-        }
-        
+    public DbSet<State> States { get; set; }
+
+    public DbSet<Priority> Prioritys { get; set; }
+
+    public DbSet<Ticket> Tickets { get; set; }
+
+    public DbSet<Post> Posts { get; set; }
+
+    public DbSet<PostHtml> PostHtmls { get; set; }
+
+    public DbSet<AttachedFile> AttachedFiles { get; set; }
+
+    public DbSet<Goal> Goals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
     }
+}
