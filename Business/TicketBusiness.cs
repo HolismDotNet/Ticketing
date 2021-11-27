658be78f1538eb919d166f8174de2852a3acb5be
Saeed Nemati <sn.1361@gmail.com>
2021-11-27 20:15:40 +0330

    committed by script

--- Business/TicketBusiness.cs
+++ Business/TicketBusiness.cs
@@ -24,6 +24,7 @@ namespace Holism.Ticketing.Business
         {
             item.RelatedItems.TimeAgo =
                 DateTime.Now.Subtract(item.UtcDate).Humanize();
+            item.RelatedItems.TitleizedStateKey = item.StateKey.Titleize();
             base.ModifyItemBeforeReturning(item);
         }
 
