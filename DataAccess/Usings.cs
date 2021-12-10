98920f2539e8af0aa5d82991c6f79a85944d1320
Saeed Nemati <sn.1361@gmail.com>
2021-12-10 17:33:04 +0330

    committed by script

---
+++ DataAccess/Usings.cs
@@ -0,0 +1,9 @@
+global using Holism.DataAccess;
+global using Holism.Infra;
+global using Holism.Models;
+global using System;
+global using System.Dynamic;
+global using System.Collections.Generic;
+global using Microsoft.EntityFrameworkCore;
+
+global using Holism.Ticketing.Models;
