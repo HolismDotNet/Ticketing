c754df7b1f606718155e27e6e7e6a5e033388e2e
Saeed Nemati <sn.1361@gmail.com>
2021-12-18 11:08:18 +0330

    committed by script

--- Api/Admin/TicketController.cs
+++ Api/Admin/TicketController.cs
@@ -5,4 +5,18 @@ public class TicketController : Controller<TicketView, Ticket>
     public override ReadBusiness<TicketView> ReadBusiness => new TicketBusiness();
     
     public override Business<TicketView, Ticket> Business => new TicketBusiness();
+
+    [HttpPost]
+    public TicketView Close(long ticketId)
+    {
+        var ticket = new TicketBusiness().CloseTicket(ticketId);
+        return ticket;
+    }
+
+    [HttpGet]
+    public TicketWithPosts View(long ticketId)
+    {
+        var ticketWithPosts = new TicketBusiness().GetTicketWithPosts(ticketId);
+        return ticketWithPosts;
+    }
 }
