38308f2aac459e8de5f24d7bc8b8044709e35d50
Saeed Nemati <sn.1361@gmail.com>
2022-01-15 15:02:21 +0330

    committed by script

--- Business/TicketBusiness.cs
+++ Business/TicketBusiness.cs
@@ -1,83 +1,75 @@
-﻿using System;
-using System.Linq.Expressions;
-using Holism.Business;
-using Holism.DataAccess;
-using Holism.Infra;
-using Holism.Ticketing.DataAccess;
-using Holism.Ticketing.Models;
-using Humanizer;
+﻿using Holism.Ticketing.DataAccess;
 
-namespace Holism.Ticketing.Business
+namespace Holism.Ticketing.Business;
+
+public class TicketBusiness : Business<TicketView, Ticket>
 {
-    public class TicketBusiness : Business<TicketView, Ticket>
+    protected override Repository<Ticket> WriteRepository =>
+        Repository.Ticket;
+
+    protected override ReadRepository<TicketView> ReadRepository =>
+        Repository.TicketView;
+
+    protected override Func<Sort> DefaultSort => () => new Sort
     {
-        protected override Repository<Ticket> WriteRepository =>
-            Repository.Ticket;
+        Property = nameof(TicketView.LatestPostUtcDate),
+        Direction = SortDirection.Descending
+    };
 
-        protected override ReadRepository<TicketView> ReadRepository =>
-            Repository.TicketView;
+    protected override void ModifyItemBeforeReturning(TicketView item)
+    {
+        item.RelatedItems.TimeAgo =
+            UniversalDateTime.Now.Subtract(item.UtcDate).Humanize();
+        item.RelatedItems.TitleizedStateKey = item.StateKey.Titleize();
+        base.ModifyItemBeforeReturning(item);
+    }
 
-        protected override Func<Sort> DefaultSort => () => new Sort
+    public Ticket Create(TicketWithBody ticketWithBody)
+    {
+        var ticket = ticketWithBody.CastTo<Ticket>();
+        ticket.RelatedItems = ticketWithBody.Body;
+        return Create(ticket);
+    }
+
+    protected override void PreCreation(Ticket ticket)
+    {
+        ticket.UtcDate = UniversalDateTime.Now;
+        ticket.TicketStateId = (int)TicketState.New;
+    }
+
+    protected override void PostCreation(Ticket ticket)
+    {
+        new PostBusiness()
+            .CreateUserResponse(ticket.Id, ticket.RelatedItems);
+    }
+
+    public TicketView CloseTicket(long ticketId)
+    {
+        return SetState(ticketId, TicketState.Closed);
+    }
+
+    public TicketView SetState(long ticketId, TicketState state)
+    {
+        var ticket = WriteRepository.Get(ticketId);
+        ticket.TicketStateId = (int)state;
+        Update (ticket);
+        return Get(ticketId);
+    }
+
+    public void EnsureTicketBelongsToUser(long ticketId, Guid userGuid)
+    {
+        var ticket = Get(ticketId);
+        if (ticket.UserGuid != userGuid)
         {
-            Property = nameof(TicketView.LatestPostUtcDate),
-            Direction = SortDirection.Descending
-        };
-
-        protected override void ModifyItemBeforeReturning(TicketView item)
-        {
-            item.RelatedItems.TimeAgo =
-                UniversalDateTime.Now.Subtract(item.UtcDate).Humanize();
-            item.RelatedItems.TitleizedStateKey = item.StateKey.Titleize();
-            base.ModifyItemBeforeReturning(item);
+            throw new ClientException($"User does not own this ticket");
         }
+    }
 
-        public Ticket Create(TicketWithBody ticketWithBody)
-        {
-            var ticket = ticketWithBody.CastTo<Ticket>();
-            ticket.RelatedItems = ticketWithBody.Body;
-            return Create(ticket);
-        }
-
-        protected override void PreCreation(Ticket ticket)
-        {
-            ticket.UtcDate = UniversalDateTime.Now;
-            ticket.TicketStateId = (int)TicketState.New;
-        }
-
-        protected override void PostCreation(Ticket ticket)
-        {
-            new PostBusiness()
-                .CreateUserResponse(ticket.Id, ticket.RelatedItems);
-        }
-
-        public TicketView CloseTicket(long ticketId)
-        {
-            return SetState(ticketId, TicketState.Closed);
-        }
-
-        public TicketView SetState(long ticketId, TicketState state)
-        {
-            var ticket = WriteRepository.Get(ticketId);
-            ticket.TicketStateId = (int)state;
-            Update (ticket);
-            return Get(ticketId);
-        }
-
-        public void EnsureTicketBelongsToUser(long ticketId, Guid userGuid)
-        {
-            var ticket = Get(ticketId);
-            if (ticket.UserGuid != userGuid)
-            {
-                throw new ClientException($"User does not own this ticket");
-            }
-        }
-
-        public TicketWithPosts GetTicketWithPosts(long ticketId)
-        {
-            var ticketWithPosts = new TicketWithPosts();
-            ticketWithPosts.Ticket = Get(ticketId);
-            ticketWithPosts.Posts = new PostBusiness().GetPosts(ticketId);
-            return ticketWithPosts;
-        }
+    public TicketWithPosts GetTicketWithPosts(long ticketId)
+    {
+        var ticketWithPosts = new TicketWithPosts();
+        ticketWithPosts.Ticket = Get(ticketId);
+        ticketWithPosts.Posts = new PostBusiness().GetPosts(ticketId);
+        return ticketWithPosts;
     }
 }
