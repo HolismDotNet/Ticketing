f793c6792ec43438f8790259f6a3bd252c789a04
Saeed Nemati <sn.1361@gmail.com>
2022-01-25 10:14:06 +0330

    committed by script

--- DataAccess/TicketingContext.cs
+++ DataAccess/TicketingContext.cs
@@ -4,15 +4,15 @@ public class TicketingContext : DatabaseContext
 {
     public override string ConnectionStringName => "Ticketing";
 
-    public DbSet<AttachedFile> AttachedFiles { get; set; }
+    public DbSet<Ticketing.AttachedFile> AttachedFiles { get; set; }
 
-    public DbSet<PostContent> PostContents { get; set; }
+    public DbSet<Ticketing.PostContent> PostContents { get; set; }
 
-    public DbSet<Post> Posts { get; set; }
+    public DbSet<Ticketing.Post> Posts { get; set; }
 
-    public DbSet<Ticket> Tickets { get; set; }
+    public DbSet<Ticketing.Ticket> Tickets { get; set; }
 
-    public DbSet<TicketView> TicketViews { get; set; }
+    public DbSet<Ticketing.TicketView> TicketViews { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
